c731507ccd17bcbe35fa807d6539a26f0ff8ee82
teshi <teshi@teshi-X230.(none)>
2013-07-12 07:14:12 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cashbook::Application.config.secret_key_base = '38f3b3b0951dd915103db5a6b202d0467946c2d9c35113d015b00df29bc8e89466e72d1fa4aa8aa150c406f2975392b9c468aeaba643ab872aaff3c52d627a3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cashbook::Application.config.session_store :cookie_store, key: '_cashbook_session'
